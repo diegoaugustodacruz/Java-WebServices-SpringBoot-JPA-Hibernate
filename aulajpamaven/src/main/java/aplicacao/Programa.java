866e5616cf0d4b27cbff7190bbbb3e2eab22de91
diego.cruz <diego.cruz@accountfy.com>
2022-01-18 14:49:16 -0300

    iniciando um projeto minimo para conceitos fundamentais

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,19 +1,43 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Joaquim Torres", "joaquin@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Ana Maria", "ana@gmail.com");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		//EXEMPLO 01 - INSERINDO NO BANCO DE DADOS
+//		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+//		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquin@gmail.com");
+//		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+//		
+//		em.getTransaction().begin();
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
 
+//		em.getTransaction().commit();
+		
+		//EXEMPLO 02 - Buscando objeto pelo ID
+//		Pessoa p = em.find(Pessoa.class, 2);
+		
+		//EXEMPLO 02 - Apagando objeto
+		em.getTransaction().begin();
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto");
+		em.close();
+		emf.close();
 
 
 		
